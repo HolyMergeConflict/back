330b6b4e9ffc2a4a0cc329ccdcc6dcdfc274e8f5
FlintWithBlackCrown <kbolokhov@gmail.com>
2025-08-02 18:42:47 +0300

    убрал логи и users_router из-за ненадобности

--- app/api/v1/routers/users_router.py
+++ app/api/v1/routers/users_router.py
@@ -6,13 +6,11 @@ from sqlalchemy.orm import Session
 from app.auth.security import get_current_user
 from app.database import get_db
 from app.enums.user_role import UserRoleEnum
-from app.logger import setup_logger
 from app.models.user_table import User
 from app.schemas.user import  UserPublic, UserUpdate
 from app.services.user_service import UserService
 
 router = APIRouter(prefix="/users", tags=["users"])
-logger = setup_logger(__name__)
 
 @router.get('/', response_model= List[UserPublic])
 def get_users(
@@ -21,7 +19,6 @@ def get_users(
         current_user: User = Depends(get_current_user)
 ):
     user_service = UserService(db)
-    logger.info('Starting to get users, request passed to service')
     if role:
         return user_service.get_users_by_role(role, current_user)
     if role is None:
@@ -40,7 +37,6 @@ def get_user(
         current_user: User = Depends(get_current_user)
 ):
     user_service = UserService(db)
-    logger.info(f'Starting to get user with id: ${user_id}, request passed to service')
     return user_service.get_user(user_id, current_user)
 
 @router.put('/{user_id}', response_model=UserPublic)
@@ -51,7 +47,6 @@ def update_user(
         current_user: User = Depends(get_current_user)
 ):
     user_service = UserService(db)
-    logger.info(f'Starting to update user with id :${user_id}, request passed to service')
     return user_service.update_user(user_id, user_data.model_dump(), current_user)
 
 @router.patch('/{user_id}/role', response_model=UserPublic)
@@ -62,7 +57,6 @@ def update_user_roles(
         current_user: User = Depends(get_current_user)
 ):
     user_service = UserService(db)
-    logger.info('Starting to update user role, request passed to service')
     return user_service.update_user_role(user_id, new_role, current_user)
 
 @router.patch("/{user_id}/promote/moderator", response_model=UserPublic)
@@ -72,7 +66,6 @@ def promote_to_moderator(
         current_user: User = Depends(get_current_user)
 ):
     user_service = UserService(db)
-    logger.info('Starting to promote user to moderator, request passed to service')
     return user_service.promote_to_moderator(user_id, current_user)
 
 @router.patch("/{user_id}/promote/admin", response_model=UserPublic)
@@ -82,7 +75,6 @@ def promote_to_admin(
         current_user: User = Depends(get_current_user)
 ):
     user_service = UserService(db)
-    logger.info('Starting to promote user to admin, request passed to service')
     return user_service.promote_to_admin(user_id, current_user)
 
 @router.patch("/{user_id}/demote", response_model=UserPublic)
@@ -93,7 +85,6 @@ def demote_user(
         current_user: User = Depends(get_current_user)
 ):
     user_service = UserService(db)
-    logger.info('Starting to demote user, request passed to service')
     return user_service.demote_user(user_id, current_user, new_role)
 
 @router.delete('/{user_id}', status_code=status.HTTP_204_NO_CONTENT)
@@ -103,5 +94,4 @@ def delete_user(
         current_user: User = Depends(get_current_user)
 ):
     user_service = UserService(db)
-    logger.info(f'Starting to delete user with id: ${user_id}, request passed to service')
     user_service.delete_user_by_id(user_id, current_user)
