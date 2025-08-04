2cd0026d23c89db8596b39730791866107ac9c71
FlintWithBlackCrown <kbolokhov@gmail.com>
2025-08-04 11:47:02 +0300

    перевел CRUD в async

--- app/db/CRUD/task_history.py
+++ app/db/CRUD/task_history.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from typing import override
 
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
 from app.db.CRUD.CRUD_base import CRUDBase
@@ -8,34 +10,44 @@ from app.models.task_history_table import TaskHistory
 
 
 class TaskHistoryCRUD(CRUDBase[TaskHistory]):
-    def __init__(self, db):
+    def __init__(self, db: AsyncSession):
         super().__init__(db, TaskHistory)
 
 
-    def get_by_user_and_task(self, user_id: int, task_id: int) -> list[TaskHistory]:
-        return self.get_all(user_id=user_id, task_id=task_id)
+    async def get_by_user_and_task(self, user_id: int, task_id: int) -> list[TaskHistory]:
+        return await self.get_all(user_id=user_id, task_id=task_id)
 
 
-    def get_in_time_range(self, user_id: int, start_time: datetime, end_time: datetime) -> list[TaskHistory]:
-        return self.get_all(self.model.timestamp >= start_time, self.model.timestamp <= end_time, user_id=user_id)
+    async def get_in_time_range(self, user_id: int, start_time: datetime, end_time: datetime) -> list[TaskHistory]:
+        return await self.get_all(
+            self.model.timestamp >= start_time,
+            self.model.timestamp <= end_time,
+            user_id=user_id
+        )
 
-
-    def get_latest_attempt(self, user_id: int, task_id: int) -> TaskHistory | None:
-        return (self.db
-                .query(self.model)
-                .filter_by(user_id=user_id, task_id=task_id)
-                .order_by(self.model.timestamp.desc())
-                .first()
-                )
+    async def get_latest_attempt(self, user_id: int, task_id: int) -> TaskHistory | None:
+        stmt = (
+            select(self.model)
+            .filter_by(user_id=user_id, task_id=task_id)
+            .order_by(self.model.timestamp.desc())
+            .limit(1)
+        )
+        result = await self.db.execute(stmt)
+        return result.scalars().first()
 
     @override
-    def create(self, obj: TaskHistory) -> TaskHistory:
+    async def create(self, obj: TaskHistory) -> TaskHistory:
         self.db.add(obj)
-        self.db.commit()
-        self.db.refresh(obj)
+        await self.db.commit()
+        await self.db.refresh(obj)
 
-
-        return self.db.query(self.model).options(
-            joinedload(self.model.user),
-            joinedload(self.model.task)
-        ).filter(self.model.id == obj.id).first()
+        stmt = (
+            select(self.model)
+            .options(
+                joinedload(self.model.user),
+                joinedload(self.model.task)
+            )
+            .filter(self.model.id == obj.id)
+        )
+        result = await self.db.execute(stmt)
+        return result.scalars().first()
