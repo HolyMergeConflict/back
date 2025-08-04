19efa27c2d7368dc31e94785e17cc85b4ac93bdd
FlintWithBlackCrown <kbolokhov@gmail.com>
2025-08-04 14:54:51 +0300

    зарефакторил декоратор

--- app/decorators/role_decorators.py
+++ app/decorators/role_decorators.py
@@ -1,20 +1,15 @@
 from functools import wraps
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, Depends
 
+from app.auth.security import get_current_user
 from app.enums.user_role import UserRoleEnum
+from app.models.user_table import User
 
 
 def require_role(*allowed_roles: UserRoleEnum):
     def decorator(func):
         @wraps(func)
-        async def wrapper(*args, **kwargs):
-            current_user = kwargs.get('current_user')
-            if current_user is None:
-                raise HTTPException(
-                    status_code=status.HTTP_401_UNAUTHORIZED,
-                    detail='Unauthorized',
-                )
-
+        async def wrapper(*args, current_user: User = Depends(get_current_user), **kwargs):
             if current_user.role not in allowed_roles:
                 raise HTTPException(
                     status_code=status.HTTP_403_FORBIDDEN,
