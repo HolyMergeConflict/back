b69b3622339dff32ae83529969f45a3f1fd5d253
FlintWithBlackCrown <kbolokhov@gmail.com>
2025-08-04 12:07:10 +0300

    перевел user_service в async

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.CRUD.user import UserCRUD
 from app.enums.user_role import UserRoleEnum
@@ -8,60 +8,60 @@ from app.logger import setup_logger
 from app.models.user_table import User
 
 class UserService:
-    def __init__(self, db: Session):
+    def __init__(self, db: AsyncSession):
         self.user_crud = UserCRUD(db)
         self.logger = setup_logger(__name__)
 
 
-    def create_user(self, user_data: dict, creator: User = None) -> User:
+    async def create_user(self, user_data: dict, creator: User = None) -> User:
         role = user_data.get('role', UserRoleEnum.STUDENT)
-        self.logger.info(f'Creating user with role: {user_data['role']}')
+        self.logger.info(f'Creating user with role: {role}')
 
         self._validate_role_assignment(role, creator)
 
-        if self.user_crud.get_user_by_email(user_data.get('email')):
+        if await  self.user_crud.get_user_by_email(user_data.get('email')):
             self.logger.exception(f'User with email {user_data['email']} already exists',)
             raise EmailAlreadyRegistered()
 
-        if self.user_crud.get_user_by_username(user_data.get('username')):
+        if await self.user_crud.get_user_by_username(user_data.get('username')):
             self.logger.exception(f'User with username {user_data['username']} already exists')
             raise UsernameAlreadyTaken()
 
         user = User(**user_data)
-        created_user = self.user_crud.create(user)
+        created_user = await self.user_crud.create(user)
         self.logger.info(f'User created successfully with id: {created_user.id}')
 
         return created_user
 
 
-    def update_user_role(self, user_id: int, new_role: UserRoleEnum, admin: User) -> User:
-        self.logger.info('Updating user role for user with id: ', user_id)
+    async def update_user_role(self, user_id: int, new_role: UserRoleEnum, admin: User) -> User:
+        self.logger.info(f'Updating user role for user with id: {user_id}')
         if not self._is_admin(admin):
             self.logger.exception('Only admins can update user roles')
             raise PermissionDeniedUser()
 
-        user = self.user_crud.get_one(id=user_id)
+        user = await self.user_crud.get_one(id=user_id)
         if not user:
             self.logger.exception('User not found')
             raise UserNotFound()
 
         self._validate_role_assignment(new_role, admin)
 
-        updated_user = self.user_crud.update(user, {'role': new_role})
+        updated_user = await self.user_crud.update(user, {'role': new_role})
         self.logger.info(f'User role updated successfully for user with id: %s, by admin with id: {admin.id}', )
         return updated_user
 
 
-    def update_user(self, user_id: int, user_data: dict, requesting_user: User) -> User:
+    async def update_user(self, user_id: int, user_data: dict, requesting_user: User) -> User:
         if requesting_user.id == user_id:
             self.logger.info(f'Updating user with id: {user_id}')
-            return self.user_crud.update(requesting_user, user_data)
+            return await self.user_crud.update(requesting_user, user_data)
 
         if requesting_user.id != user_id:
             if self._is_admin(requesting_user):
-                if user := self.user_crud.get_one(id=user_id):
+                if user := await self.user_crud.get_one(id=user_id):
                     self.logger.info(f'Updating user with id: {user_id}')
-                    return self.user_crud.update(user, user_data)
+                    return await self.user_crud.update(user, user_data)
                 else:
                     self.logger.exception('User not found')
                     raise UserNotFound()
@@ -70,27 +70,27 @@ class UserService:
                 raise PermissionDeniedUser()
 
 
-    def promote_to_moderator(self, user_id: int, requesting_user: User) -> User:
+    async def promote_to_moderator(self, user_id: int, requesting_user: User) -> User:
         self.logger.info(f'Promoting user to moderator: {user_id}')
-        return self._change_user_role(user_id, UserRoleEnum.MODERATOR, requesting_user)
+        return await self._change_user_role(user_id, UserRoleEnum.MODERATOR, requesting_user)
 
 
-    def promote_to_admin(self, user_id: int, admin: User) -> User:
+    async def promote_to_admin(self, user_id: int, admin: User) -> User:
         self.logger.info(f'Promoting user to admin: {user_id}')
-        return self._change_user_role(user_id, UserRoleEnum.ADMIN, admin)
+        return await self._change_user_role(user_id, UserRoleEnum.ADMIN, admin)
 
 
-    def demote_user(self, user_id: int, requesting_user: User, new_role: UserRoleEnum = UserRoleEnum.STUDENT) -> User:
+    async def demote_user(self, user_id: int, requesting_user: User, new_role: UserRoleEnum = UserRoleEnum.STUDENT) -> User:
         self.logger.info(f'Demoting user with id: {user_id} to role: {new_role}')
-        return self._change_user_role(user_id, new_role, requesting_user)
+        return await self._change_user_role(user_id, new_role, requesting_user)
 
 
-    def get_self_user_by_id(self, user_id: int) -> User:
-        self.logger.info('Getting user by id: ', user_id)
-        return self.user_crud.get_one(id=user_id)
+    async def get_self_user_by_id(self, user_id: int) -> User:
+        self.logger.info(f'Getting user by id: {user_id}')
+        return await self.user_crud.get_one(id=user_id)
 
 
-    def get_user(self, user_id: int, requesting_user: User) -> User:
+    async def get_user(self, user_id: int, requesting_user: User) -> User:
         self.logger.info('starting get user')
         if user_id == requesting_user.id:
             self.logger.info(f'Getting user by id: {user_id}')
@@ -98,9 +98,9 @@ class UserService:
         if user_id != requesting_user.id:
             if self._can_view_users(requesting_user):
                 self.logger.info(f'Getting user by id: {user_id}')
-                if self.user_crud.get_one(id=user_id):
+                if user := await self.user_crud.get_one(id=user_id):
                     self.logger.info(f'User found: {user_id}')
-                    return self.user_crud.get_one(id=user_id)
+                    return user
                 else:
                     self.logger.exception(f'User not found: {user_id}')
                     raise UserNotFound()
@@ -109,26 +109,26 @@ class UserService:
                 raise PermissionDeniedUser()
 
 
-    def get_users(self, requesting_user: User, **filters) -> list[User]:
+    async def get_users(self, requesting_user: User, **filters) -> list[User]:
         if not self._can_view_users(requesting_user):
             self.logger.exception(f'Insufficient permissions to view users: {requesting_user.id}')
             raise PermissionDeniedUser()
         self.logger.info(f'Getting users by {requesting_user.id}')
-        return self.user_crud.get_all(**filters)
+        return await self.user_crud.get_all(**filters)
 
 
-    def get_users_by_role(self, role: UserRoleEnum, requesting_user: User) -> list[User]:
+    async def get_users_by_role(self, role: UserRoleEnum, requesting_user: User) -> list[User]:
         self.logger.info(f'Getting users by role: {role}, requesting user: {requesting_user.id}')
-        return self.get_users(requesting_user, role=role)
+        return await self.get_users(requesting_user, role=role)
 
 
-    def _change_user_role(self, user_id: int, new_role: UserRoleEnum, requesting_user: User) -> User:
+    async def _change_user_role(self, user_id: int, new_role: UserRoleEnum, requesting_user: User) -> User:
         if not self._is_admin(requesting_user):
             self.logger.exception(f'Only administrators can change user roles, requesting user: {requesting_user.id}')
             raise PermissionDeniedUser()
 
 
-        user = self.user_crud.get_one(id=user_id)
+        user = await self.user_crud.get_one(id=user_id)
         if not user:
             self.logger.exception(f'User not found, id: {user_id}')
             raise UserNotFound()
@@ -137,7 +137,7 @@ class UserService:
             self.logger.exception(f'Administrators cannot demote themselves, requesting user: {requesting_user.id}')
             raise CannotDemoteSelf()
 
-        updated_user = self.user_crud.update(user, {'role': new_role})
+        updated_user = await self.user_crud.update(user, {'role': new_role})
         self.logger.info(f'User role updated successfully for user with id: ${user_id}, by admin with id: {requesting_user.id}')
         return updated_user
 
@@ -149,25 +149,25 @@ class UserService:
             raise PermissionDeniedUser()
 
 
-    def delete_user(self, user: User, requesting_user: User) -> None:
+    async def delete_user(self, user: User, requesting_user: User) -> None:
         if not self._is_admin(requesting_user):
             self.logger.exception(f'Only administrators can delete users, requesting user: {requesting_user.id}')
             raise PermissionDeniedUser()
 
         if not user:
             self.logger.exception(f'User not found, id: {user.id}')
-            raise UserNotFound
+            raise UserNotFound()
         self.logger.info(f'Deleting user with id: {user.id}')
-        return self.user_crud.delete(user)
+        return await self.user_crud.delete(user)
 
 
-    def delete_user_by_id(self, user_id: int, requesting_by: User) -> None:
-        if user := self.user_crud.get_one(id=user_id):
-            self.logger.info(f'Deleting user with id: {user.id}')
-            self.delete_user(user, requesting_by)
+    async def delete_user_by_id(self, user_id: int, requesting_by: User) -> None:
+        if user := await self.user_crud.get_one(id=user_id):
+            self.logger.info(f'Deleting user with id: {user_id}')
+            await self.delete_user(user, requesting_by)
         else:
-            self.logger.exception(f'User not found, id: {user.id}')
-            raise UserNotFound
+            self.logger.exception(f'User not found, id: {user_id}')
+            raise UserNotFound()
 
 
     @staticmethod
